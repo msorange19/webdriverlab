b35154db553216a0d14eb970c85b77e506ed4996
msorange19 <muhaiminul3048@gmail.com>
2025-01-08 19:13:21 +0600

    data

---
+++ nopstation_webdriverio/test/pageobjects/login.js
@@ -0,0 +1,24 @@
+export default class LoginView {
+    get usernameField() {
+        return $('~test-Username');
+    }
+
+    get passwordField() {
+        return $('~test-Password');
+    }
+
+    /*get autoFilled() {
+        return $('//android.widget.TextView[@text="standard_user"]');
+    }
+*/
+    get loginButton() {
+        return $('~test-LOGIN');
+    }
+
+    async verifyLogin(username, password) {
+        await this.usernameField.setValue(username);
+        await this.passwordField.setValue(password);
+        //await this.autoFilled.click();
+        await this.loginButton.click();
+    }
+}
---
+++ nopstation_webdriverio/test/specs/test.js
@@ -0,0 +1,12 @@
+import LoginView from '../pageobjects/login.js';
+import testData  from '../data.json'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+       // await LoginPage.open()
+        const loginPage = new LoginView();
+        await loginPage.verifyLogin(testData.username, testData.password);
+
+    })
+})
+
